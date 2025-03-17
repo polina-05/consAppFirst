de72bd7126dcbf8b4f9c2aa49bae9834376f1723
Polina <polina.23qw@gmail.com>
2025-03-17 13:56:49 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+console.log(personalMovieDB);
