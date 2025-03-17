c374d9412b470a2d2c0e948a241154033b68e6a5
Polina <polina.23qw@gmail.com>
2025-03-17 14:52:40 +0200

    part 1 - add answers in obj Movie

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+"use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -7,5 +9,13 @@ const personalMovieDB = {
   genres: [],
   privat: false,
 };
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
